c822adcd04e5ec27627b48fff8f0f48df083c0a9
= <za.qa@outlook.com>
2020-11-09 16:46:32 +0000

    tables created

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const { router } = require("./routes/data");
+const port = 5000;
 
-const indexRouter = require("./routes/index");
-
+//Initialise app
 const app = express();
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//hand the router to app
+app.use("/", router);
 
-app.use("/", indexRouter);
-
-module.exports = app;
+app.listen(port, function () {
+  console.log(`Listenting on port ${port}`);
+});
