afa7dc0aa17337130ab91ad6ad25b29c0e331f68
soupanova <duykhangkhuat@gmail.com>
2020-11-13 11:01:07 +0000

    collection update

--- db/collection.js
+++ db/collection.js
@@ -4,38 +4,161 @@ const mentees = [
     lastName: "Khuat",
     introduction:
       "Hi, I'm Khang and I'm a chef and spent most of my time improving my creativity through culinary arts. I now want to channel my passion for food into programming for the future.",
-    myersBriggs: "INFP",
-    industry: "Games Development",
-    interests: "Music",
+    myersBriggs: "INFJ",
+    industry: "Open",
+    interests: "Art, Games, Music, Food",
+    gpic: "https://i.imgur.com/avrFKI3.png",
+    gtime: "13/11/2020, 09:40",
+    gemail: "duykhangkhuat@gmail.com",
   },
   {
     firstName: "Ismail",
     lastName: "Ali",
-    introduction: "I'm the best swimmer in Birmingham, no cap",
+    introduction:
+      "Hi, I'm Ismail. I wanted to learn a new skillset during lockdown and I started coding, I'm still currently new to it but I want to improve and be a part of the tech industry. Also I'm the best swimmer in Birmingham, no cap",
     myersBriggs: "ISTJ",
     industry: "DevOps",
-    interests: "Sports",
+    interests: "Food, Sports",
+    gpic: "https://i.imgur.com/ZywiEi4.png",
+    gtime: "13/11/2020, 09:46",
+    gemail: "ismaila@gmail.com",
+  },
+  {
+    firstName: "Piper",
+    lastName: "Bates",
+    introduction:
+      "I'm Piper, & I'm passionate about creativity. I've been an illustrator for the past 5 years, & now I'm diving into the exciting world of code. Watch this space to see what I end up making with my new found programming skills!",
+    myersBriggs: "INFP",
+    industry: "Games Development, DevOps, UX",
+    interests: "Art",
+    gpic: "https://i.imgur.com/eCtaKdS.png",
+    gtime: "13/11/2020, 09:47",
+    gemail: "piperbates@gmail.com",
+  },
+  {
+    firstName: "Kunmi",
+    lastName: "Williams",
+    introduction: `Hiya, I'm Kunmi, I've have seen the impact the tech industry can have and the power and flexibiilty programming provides us, I wanted to be a part of the ever expanding tech industry and learn programming to help our current climate conditions! `,
+    myersBriggs: "INFP",
+    industry: "Open",
+    interests: "Art, Music",
+    gpic: "https://i.imgur.com/tCWiZqO.png",
+    gtime: "13/11/2020, 09:51",
+    gemail: "kunmiwilliams@gmail.com",
+  },
+  {
+    firstName: "Zaid",
+    lastName: "Qarout",
+    introduction:
+      "Hi, I studied visual communication design and wanted to branch and broaden my knowledge in the tech industry",
+    myersBriggs: "INFP",
+    industry: "Games Development, DevOps, UX",
+    interests: "Games, Art",
+    gpic:
+      "https://lh6.googleusercontent.com/-d3KqqRi29Qg/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucnGI3N06cxLPFOBD5AugyD84fITIQ/s96-c/photo.jpg",
+    gtime: "13/11/2020, 10:29",
+    gemail: "zaidqarout@gmail.com",
   },
 ];
 
 const mentors = [
   {
-    firstName: "Bob",
-    lastName: "Smite",
-    introduction: "I'm everywhere",
-    languages: "JavaScript",
-    myersBriggs: "ISTJ",
-    industry: "DevOps",
-    interests: "Sports",
+    firstName: "Marley",
+    lastName: "Howell",
+    introduction:
+      "Hi, I'm Marley and I've been in the tech and gaming industry for 3 years, I have much to learn and I will continue to up-skill and get familiar with the code base, culture and tools within my current work place. I hope my current knowledge will be sufficient for the new and upcoming programmers of SoC ",
+    languages: "JavaScript, PHP, Python",
+    myersBriggs: "ESTJ",
+    industry: "Games Development, UX",
+    interests: "Art, Games, Music",
+    gpic:
+      "http://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png",
+    gtime: "11/11/2020, 09:01",
+    gemail: "marleyhowell8@gmail.com",
   },
   {
-    firstName: "Boris",
-    lastName: "Johnson",
-    introduction: "I,I,I,I,I,I am,am, terribly incompetant",
-    languages: "C#",
-    myersBriggs: "ENTP",
-    industry: "Project Manager",
+    firstName: "Layla",
+    lastName: "Lee",
+    introduction:
+      "My name is Layla and I've been working on UX design for 5 years and I hope to provide my knowledge teaching new minds the joys of UX and what the tech industry can offer.",
+    languages: "C#, JavaScript, PHP",
+    myersBriggs: "ESFP",
+    industry: "UX",
+    interests: "Art, Music, Sports",
+    gpic:
+      "http://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png",
+    gtime: "11/11/2020, 09:11",
+    gemail: "laylalee420@gmail.com",
+  },
+  {
+    firstName: "Daniella",
+    lastName: "Hurst",
+    introduction:
+      "Hi, my name is Daniella and I am currently a DevOps engineer and have been working in this field for 2 years, I hope to help in anyway I can. ",
+    languages: "C/C++, C#, JavaScript, PHP",
+    myersBriggs: "ISFP",
+    industry: "DevOps",
     interests: "Sports",
+    gpic:
+      "http://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png",
+    gtime: "11/11/2020, 10:39",
+    gemail: "dani_hurst12@gmail.com",
+  },
+  {
+    firstName: "Kie",
+    lastName: "Goodman",
+    introduction:
+      "Hi, I'm Kie Goodman, feel free to ask me any question you might have in regards to my field",
+    languages: "JavaScript",
+    myersBriggs: "INFP",
+    industry: "Content Management",
+    interests: "Music",
+    gpic:
+      "http://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png",
+    gtime: "12/11/2020, 11:42",
+    gemail: "kiegoodman09@gmail.com",
+  },
+  {
+    firstName: "Aalia",
+    lastName: "Begum",
+    introduction:
+      "Hi, my name is aalia, I am self taught in my programming knowledge and have solidified my understanding and knowledge of the langauges working in several compaines with the tech industry. I hope I can provide some of my knowledge and help people going through their first learning experience of programming",
+    languages: "C/C++, C#, JavaScript",
+    myersBriggs: "INFP",
+    industry: "Web Design",
+    interests: "Games, Art",
+    gpic:
+      "http://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png",
+    gtime: "12/11/2020, 16:03",
+    gemail: "aalia-begum99@gmail.com",
+  },
+  {
+    firstName: "Mo",
+    lastName: "Fazar",
+    introduction:
+      "Hi, I'm Mo, I'm relatively new in the tech industry but I hope I can be of assistance ",
+    languages: "JavaScript",
+    myersBriggs: "INFP",
+    industry: "IT Sales",
+    interests: "Games, Art",
+    gpic:
+      "http://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png",
+    gtime: "12/11/2020, 17:47",
+    gemail: "mohammadfazar45@gmail.com",
+  },
+  {
+    firstName: "Abu",
+    lastName: "Khan",
+    introduction:
+      "I'm Abu and I've worked for 5 years as an IT Consultant and recently started in the DevOps sector.",
+    languages: "C#, PHP, Python",
+    myersBriggs: "INFP",
+    industry: "IT Consultancy, DevOps",
+    interests: "Games, Art",
+    gpic:
+      "http://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png",
+    gtime: "13/11/2020, 20:20",
+    gemail: "abu_khan_94@gmail.com",
   },
 ];
 
