4fa553a3a39117b2011e3fa64445049d92e20ef2
= <za.qa@outlook.com>
2020-11-14 19:36:03 +0000

    final

--- db/scripts/teePopulateTable.js
+++ db/scripts/teePopulateTable.js
@@ -1,15 +1,21 @@
 const { query } = require("../index");
-
 let { mentees } = require("../collection");
-
 async function populateMentees() {
-  const sql = `INSERT INTO menteeTable (firstName ,
+  const sql = `INSERT INTO menteeTable (
+    firstName ,
     lastName ,
     introduction ,
     myersBriggs ,
     industry ,
-    interests)
-     VALUES ($1, $2, $3, $4, $5, $6)`;
+    interests ,
+    gpic,
+    gid,
+    gname,
+    gtime,
+    gemail
+    )
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+  `;
   mentees.map(async function (value) {
     let res = await query(sql, [
       value.firstName,
@@ -18,6 +24,11 @@ async function populateMentees() {
       value.myersBriggs,
       value.industry,
       value.interests,
+      value.gpic,
+      value.gid,
+      value.gname,
+      value.gtime,
+      value.gemail,
     ]);
     console.log(res);
   });
--- db/scripts/torPopulateTable.js
+++ db/scripts/torPopulateTable.js
@@ -1,19 +1,25 @@
 const { query } = require("../index");
-
 let { mentors } = require("../collection");
 async function populateMentors() {
   mentors.map(async function (value) {
     let res = await query(
       `
-            INSERT INTO mentorTable (firstName,
-              lastName,
-              introduction,
-              languages,
-              myersBriggs,
-              industry,
-              interests)
-            VALUES ($1, $2, $3, $4, $5, $6, $7)
-        `,
+INSERT INTO mentorTable (
+  firstName,
+  lastName,
+  introduction,
+  languages,
+  myersBriggs,
+  industry,
+  interests,
+  gpic,
+  gid,
+  gname,
+  gtime,
+  gemail
+  )
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+`,
       [
         value.firstName,
         value.lastName,
@@ -22,6 +28,11 @@ async function populateMentors() {
         value.myersBriggs,
         value.industry,
         value.interests,
+        value.gpic,
+        value.gid,
+        value.gname,
+        value.gtime,
+        value.gemail,
       ]
     );
     console.log(res);
