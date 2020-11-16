08ec2ac17dc10a87cd3c399f53e033bfd6701d0c
Ismail Ali <ismailali@ismails-mbp.home>
2020-11-16 15:50:31 +0000

    heroku

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,git 
+  connectionString: process.env.DATABASE_URL, 
   ssl: {
     rejectUnauthorized: false,
   },
